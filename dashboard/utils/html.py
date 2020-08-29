50ed5465e1ccb3c6b1177162f284249d8518e1a0
Daniel <tang.d@hotmail.com>
2020-08-29 15:23:04 +0800

    Add tutorial implementation of dashboard

---
+++ dashboard/utils/html.py
@@ -0,0 +1,13 @@
+import dash_html_components as html
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
