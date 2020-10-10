fc8082ae74afa9442640186fb02478b7f478dcab
Daniel <tang.d@hotmail.com>
2020-10-10 14:11:51 +0800

    Push updates to unified pipeline code.

---
+++ pipeline/classify.py
@@ -0,0 +1,140 @@
+### CLASSIFICATION ###
+print('classifier.py')
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_array, check_is_fitted
+import numpy as np
+
+class CosineMeanClassifier(BaseEstimator, ClassifierMixin):
+    """ 
+    https://scikit-learn.org/stable/developers/develop.html
+    sci-kit Learn compatible classifier that implements classification based on cosine similarity.
+
+    Classifier is fit to a n x m numpy vector (n = number of observations, m = number of column vectors)
+
+    Parameters
+    ----------
+    cut_off : float, default=0.5
+        A parameter used to determine cut-off threshold for prediction.
+    Attributes
+    ----------
+    n_features_ : int
+        The number of features of the data passed to :meth:`fit`.
+    mean_vector : np.array
+        The mean vector of the input data fitted to the classifier.
+
+    Example usage:
+    >> from capstone import CosineMeanClassifier     
+    >> from sklearn.feature_extraction.text import TfidfVectorizer
+    >> from sklearn.decomposition import TruncatedSVD
+    >> from sklearn.pipeline import Pipeline
+
+    >> cosine_pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(min_df=2, tokenizer = nlp, sublinear_tf = True)),  # outputs (n x v) array
+        ('svd', TruncatedSVD(n_components=50)),  # outputs n x 50 array
+        ('cosine', CosineMeanClassifier(cut_off=0.5))
+    ])
+
+    >> cosine_pipeline.fit(event_text)  # event text is a pd.Series or np.array of strings
+    Pipeline(steps=[('tfidf',
+                 TfidfVectorizer(min_df=2, sublinear_tf=True,
+                                 tokenizer=<spacy.lang.en.English object at 0x7f7de6d47f70>)),
+                ('svd', TruncatedSVD(n_components=50)),
+                ('cosine', CosineMeanClassifier())])
+
+    >> cosine_pipeline.predict(df.event_text.values)
+    array([0, 0, 0, ..., 0, 0, 1])
+    """
+
+    def __init__(self, cutoff=0.5):
+        self.cutoff = cutoff  # threshold cut-off for classification : cosine distance is bounded [0,1]
+
+    def fit(self, X, y=None):  # Does not involve labels - we are just using mean vector like a nearest neighbor
+        """A reference implementation of a fitting function for a transformer.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        y : None
+            There is no need of a target in a transformer, yet the pipeline API
+            requires this parameter.
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        self.mean_vector = X.mean(axis=0)
+        self.n_features_ = X.shape[1]
+        return self # Return the classifier
+    
+    def transform(self, X, y=None):
+        """ A reference implementation of a transform function.
+        Parameters
+        ----------
+        X : {array-like, sparse-matrix}, shape (n_samples, n_features)
+            The input samples.
+        Returns
+        -------
+        X_transformed : array, shape (n_samples, 1)
+            The array containing the cosine similarities of the input samples.
+            in ``X``.
+        """
+        # Check is fit had been called
+        check_is_fitted(self, 'n_features_')
+        X = check_array(X)  # input validation
+        # Check that the input is of the same shape as the one passed
+        # during fit.
+        if X.shape[1] != self.n_features_:
+            raise ValueError('Shape of input is different from what was seen'
+                             'in `fit`')
+            
+        # compute cosine similarity
+        magnitude = lambda x : np.sqrt(np.sum(np.power(x,2)))  # utility function to calculate magnitude
+        similarity = lambda a, b : (np.dot(a, b)) / (magnitude(a)*magnitude(b)) 
+        
+        return np.array([similarity(x, self.mean_vector) for x in X])
+    
+    def predict_proba(self, X):
+        """ A reference implementation of a transform function.
+        Parameters
+        ----------
+        X : {array-like, sparse-matrix}, shape (n_samples, n_features)
+            The input samples.
+        Returns
+        -------
+        X_transformed : array, shape (n_samples, 1)
+            The array containing the cosine similarities of the input samples.
+            in ``X``.
+        """
+        return self.transform(X)
+    
+    def predict(self, X):
+        """ A reference implementation of a transform function.
+        Parameters
+        ----------
+        X : {array-like, sparse-matrix}, shape (n_samples, n_features)
+            The input samples.
+        Returns
+        -------
+        X_transformed : array, shape (n_samples, 1)
+            The array containing a predictions based on taking a boolean condition cosine similarities of the input samples
+            in ``X``. If the cosine similiarity is greater than self.cutoff, then return 1, else 0.
+        """
+        # Check is fit had been called
+        check_is_fitted(self, 'n_features_')
+        X = check_array(X)  # input validation
+        # Check that the input is of the same shape as the one passed
+        # during fit.
+        if X.shape[1] != self.n_features_:
+            raise ValueError('Shape of input is different from what was seen'
+                             'in `fit`')
+
+        # compute cosine similarity
+        magnitude = lambda x : np.sqrt(np.sum(np.power(x,2)))  # utility function to calculate magnitude
+        similarity = lambda a, b : (np.dot(a, b)) / (magnitude(a)*magnitude(b)) 
+        
+        # apply mean vertically over col and return a (1,n) vector,  return binary integer array
+        return np.array([(similarity(x, self.mean_vector) > self.cutoff) for x in X], dtype=np.int64)
+    
+    def fit_transform(self, X, y=None):
+        self.fit(X)
+        return self.transform(X)
--- pipeline/vectorize.py
+++ pipeline/vectorize.py
@@ -2,18 +2,17 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-import pandas as pd
 from pathlib import Path
 
+import pandas as pd
 import numpy as np
 
 import spacy
-
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 import click
 
-BASE_PATH = Path('..')
+BASE_PATH = Path('.')
 events_path = BASE_PATH / 'events'
 dictionary_path = BASE_PATH / 'dictionary'
 data_path = BASE_PATH / 'data'
@@ -23,79 +22,107 @@ df_path = data_path / 'dataframes'
 patterns_path = dictionary_path / 'patterns'
 triggers_path = dictionary_path / 'trigger phrases'
 
-from sklearn.preprocessing import MultiLabelBinarizer
+# should we save d2v.model to a model folder for better structure?
 
-mlb = MultiLabelBinarizer()
+def train_doc2vec(df, tag_col='event_id', token_col='tokens', max_epochs=100,
+                min_count=2, vec_size=50, alpha=0.025, min_alpha=0.00025, dm=1,
+                model_path="d2v.model", num_workers=4, save=False):
+    # produce a mapping tagging all text with their ids
+    assert type(df) == pd.DataFrame, 'Input data must be a dataframe with tag_col and token_col specified, or a dict.'
 
-nlp = spacy.load("en_core_web_lg")
+    tagged_data = [TaggedDocument(words=getattr(row, token_col), tags=[getattr(row, tag_col)]) for row in df.itertuples()]
 
+    # train model on tagged data
+    model = Doc2Vec(tagged_data, vector_size=vec_size, alpha=alpha, min_alpha=min_alpha, min_count=min_count,
+                dm=dm, epochs=max_epochs, workers=num_workers)
 
-def get_tokens(doc):
-    return [w.lemma_ for w in nlp(doc) if (w.is_alpha and not w.is_stop)]
+    if save:
+        print(f'Saving model to {model_path}')
+        model.save(model_path)
 
+    return model
 
-def get_vectors(df, max_epochs=100, vec_size=20, alpha=0.025, model_path="d2v.model"):
-    tagged_data = [TaggedDocument(words=row.tokens, tags=[row.event_id]) for row in df.itertuples()]
-    model = Doc2Vec(size=vec_size, alpha=alpha, min_alpha=0.00025, min_count=1, dm=1)
-    model.build_vocab(tagged_data)
-    for epoch in range(max_epochs):
-        model.train(tagged_data, total_examples=model.corpus_count, epochs=model.iter)
-        model.alpha -= 0.0002
-        model.min_alpha = model.alpha
+def load_doc2vec(model_path='d2v.model'):
+    return Doc2Vec.load(model_path)
 
-    model.save(model_path)
+def get_vecs_from_model(data, model, tag_col='event_id', as_dataframe=True):
+    # produce a mapping tagging all text with their ids
     vecs = []
-
-    for event_id in df['event_id']:
+    for tag in data[tag_col]:
         try:
-            vec = model.docvecs[event_id]
+            vec = model.docvecs[tag]
         except:
             vec = np.nan
         vecs.append(vec)
 
+    if as_dataframe:
+        return pd.DataFrame(vecs, index=data[tag_col])    
     return vecs
 
+def get_vectors(df, tag_col='event_id', token_col='tokens', max_epochs=100,
+                min_count=2, vec_size=50, alpha=0.025, min_alpha=0.00025, dm=1,
+                model_path="d2v.model", num_workers=4, load=True, save=False, as_dataframe=True):
+    if load:
+        model = load_doc2vec(model_path=model_path)
 
-def to_list(x):
-    if isinstance(x, str):
-        return x.split(',')
-    else:
-        return ['unknown']
+    else:  # train model from scratch - does not save
+        model = train_doc2vec(df, tag_col=tag_col, token_col=token_col, max_epochs=max_epochs,
+                min_count=min_count, vec_size=vec_size, alpha=alpha, min_alpha=min_alpha, dm=dm,
+                model_path=model_path, num_workers=num_workers, save=save)
+
+    return get_vecs_from_model(df, model, tag_col=tag_col, as_dataframe=as_dataframe)
+
+# not complete
+# # add one hot encoded named entity features to main df
+# def get_onehot_features(full_df, labelled_df):
+#     from sklearn.preprocessing import MultiLabelBinarizer
+#     mlb = MultiLabelBinarizer()
+
+#     def to_list(x):
+#         if isinstance(x, str):
+#             return x.split(',')
+#         else:
+#             return ['unknown']
+            
+#     for feature in ['STRAT', 'ROCK', 'LOCATION', 'MINERAL', 'ORE_DEPOSIT', 'TIMESCALE']:
+#         s = full_df[feature].apply(to_list)
+#         wide_df = pd.DataFrame(mlb.fit_transform(s), columns=mlb.classes_, index=df.index)
+#         wide_df = wide_df.add_prefix(f'{feature}_')
+#         labelled_df = labelled_df.merge(wide_df, how='left', left_index=True, right_index=True)
+#     return labelled_df
 
 
-def add_features(full_df, labelled_df):
-    for feature in ['STRAT', 'ROCK', 'LOCATION', 'MINERAL', 'ORE_DEPOSIT', 'TIMESCALE']:
-        s = full_df[feature].apply(to_list)
-        wide_df = pd.DataFrame(mlb.fit_transform(s), columns=mlb.classes_, index=df.index)
-        wide_df = wide_df.add_prefix(f'{feature}_')
-        labelled_df = labelled_df.merge(wide_df, how='left', left_index=True, right_index=True)
-    return labelled_df
+# need to combine with new vectorize.py
 
+#@click.command()
+#@click.option('--group', help='Group Number.')
+# def vectorize(group, nlp=None, load=False, save=True):    
+#     filename = events_path / f'group_{group}_labelled.csv'
+#     df = pd.read_csv(filename)
+#     df = df[df.columns]
 
-@click.command()
-@click.option('--group', help='Group Number.')
-def vectorize(group):
-    filename = events_path / f'group_{group}_labelled.csv'
-    df = pd.read_csv(filename)
-    df = df[df.columns[2:]]
+#     labelled_df = df.loc[df.reviewed][['event_id', 'event_text', 'Near Miss Event']]
 
-    labelled_df = df.loc[df.reviewed][['event_id', 'event_text', 'Near Miss Event']]
-
-    # Event Text Vector
-    labelled_df['tokens'] = labelled_df['event_text'].apply(get_tokens)
-    labelled_df['event_text_vector'] = get_vectors(labelled_df)
-
-    # One hot encoded Feature
-    labelled_df = add_features(df, labelled_df)
-
-    # Label
-    labelled_df['label'] = labelled_df['Near Miss Event'].astype(int)
-
-    # Drop unused labels
-    labelled_df = labelled_df.drop(columns=['tokens', 'event_text', 'Near Miss Event'])
-
-    output_filename = events_path / f'group_{group}_processed.csv'
-    labelled_df.to_csv(output_filename, index=False)
+#     if nlp == None:
+#         from .processing import load_spacy_model
+#         nlp = load_spacy_model(output_type='text', tokenizer_only=True, verbose=False)
+   
+#     # Event Text Vector
+#     labelled_df['tokens'] = list(nlp.pipe(labelled_df['event_text'].values))
     
-if __name__ == '__main__':
-    vectorize()
+#     get_vectors(labelled_df, load=load, save=save)
+
+#     # One hot encoded Feature
+#     labelled_df = add_features(df, labelled_df)
+
+#     # Label
+#     labelled_df['label'] = labelled_df['Near Miss Event'].astype(int)
+
+#     # Drop unused labels
+#     labelled_df = labelled_df.drop(columns=['tokens', 'event_text', 'Near Miss Event'])
+
+#     output_filename = events_path / f'group_{group}_processed.csv'
+#     labelled_df.to_csv(output_filename, index=False)
+    
+# if __name__ == '__main__':
+#     vectorize(2)
